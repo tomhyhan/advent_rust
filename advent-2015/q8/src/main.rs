6813e104214a17277739cd2b64b05f3563fde9ff
bcfsprogrammer <bcfsprogrammer@gmail.com>
2023-01-24 10:44:53 -0800

    q8

--- advent-2015/q8/src/main.rs
+++ advent-2015/q8/src/main.rs
@@ -1,13 +1,43 @@
-use std::fs::File;
-use std::io::{self, Read};
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::fs;
 
-fn read_username_from_file() -> String {
-    let mut username_file = File::open("input.txt").expect("asdf");
-    let mut username = String::new();
-    username_file.read_to_string(&mut username);
-    username
-    // Ok(username)
+fn parse(line: &str) -> usize {
+    let result = line.len();
+    let x_matches = Regex::new(r"\\x[0-9a-fA-F]{2}").unwrap();
+    let q_matches = Regex::new(r#"""#).unwrap();
+    let e_matches = Regex::new(r"\\\\").unwrap();
+
+    let new_line = line.trim_matches(|c| c == '"');
+
+    // println!("{}", line);
+    // println!("{}", line.trim_matches(|c| c == '"'));
+    // println!("{}", new_line);
+    // println!("")
+    // println!("{new_line:?}")
+
+    // matches
+    //     .captures_iter(new_line)
+    //     .for_each(|caps| println!("{:?}", caps.name("x").unwrap_or(caps.get(1).unwrap())))
+
+    let x = x_matches.find_iter(new_line).count();
+    let y = q_matches.find_iter(new_line).count();
+    let z = e_matches.find_iter(new_line).count();
+    let escapes = (x * 3) + y + z;
+    println!("{result}");
+    // println!("{}", in_memory + 2);
+    println!("{}", result - escapes - 2);
+    // println!();
+
+    escapes + 2
 }
+
 fn main() {
-    println!("{}", read_username_from_file().unwrap());
+    let path = "input.txt";
+    let context = fs::read_to_string(path).expect("file to be read");
+
+    let r: usize = context.split("\n").map(|line| parse(line)).sum();
+
+    println!("{r}");
+    println!("bvm\x28aa\\\\\"pywuhaniox\\z\\hbpdxd7mold");
 }
