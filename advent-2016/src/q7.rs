444ddfbb4d44f082d7883520d1cc1cc43d748048
Tom Han <tomhyhan@gmail.com>
2023-02-16 00:26:06 -0800

    q7

--- advent-2016/src/q7.rs
+++ advent-2016/src/q7.rs
@@ -1,84 +1,89 @@
-// https://adventures.michaelfbryan.com/posts/daily/iterators/
-struct Fibonacci {
-    curr: u32,
-    next: u32,
+use crate::common::my_modules::get_file;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Info {
+    hyper: Vec<String>,
+    abba: Vec<String>,
+}
+#[derive(Debug)]
+struct Ips {
+    info: Vec<Info>,
 }
 
-// Implement `Iterator` for `Fibonacci`.
-// The `Iterator` trait only requires a method to be defined for the `next` element.
-impl Iterator for Fibonacci {
-    // We can refer to this type using Self::Item
-    type Item = u32;
+fn parse(line: &str) -> Info {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"\[|\]").unwrap();
+    }
 
-    // Here, we define the sequence using `.curr` and `.next`.
-    // The return type is `Option<T>`:
-    //     * When the `Iterator` is finished, `None` is returned.
-    //     * Otherwise, the next value is wrapped in `Some` and returned.
-    // We use Self::Item in the return type, so we can change
-    // the type without having to update the function signatures.
-    fn next(&mut self) -> Option<Self::Item> {
-        let current = self.curr;
+    let mut hyper = Vec::new();
+    let mut abba = Vec::new();
 
-        self.curr = self.next;
-        self.next = current + self.next;
+    RE.split(line)
+        .enumerate()
+        .for_each(|(idx, s)| match idx % 2 {
+            1 => hyper.push(s.to_string()),
+            0 => abba.push(s.to_string()),
+            _ => panic!("not known"),
+        });
 
-        // Since there's no endpoint to a Fibonacci sequence, the `Iterator`
-        // will never return `None`, and `Some` is always returned.
-        Some(current)
+    Info { hyper, abba }
+}
+
+impl Ips {
+    fn tls_count(&self) -> usize {
+        self.info
+            .iter()
+            .filter(|&info| {
+                println!("{:?}", self.is_tls(info));
+                self.is_tls(info)
+            })
+            .count()
+    }
+
+    fn is_tls(&self, info: &Info) -> bool {
+        let mut hypers = info.hyper.iter();
+        while let Some(hyper) = hypers.next() {
+            // println!("{hyper:?}");
+            let hyper = hyper.as_bytes();
+
+            for abba in hyper.windows(4) {
+                if abba[0] == abba[3] && abba[1] == abba[2] {
+                    return false;
+                }
+            }
+        }
+
+        let mut abbas = info.abba.iter();
+        while let Some(abba) = abbas.next() {
+            let abba = abba.as_bytes();
+
+            for ab in abba.windows(4) {
+                if ab[0] != ab[1] && ab[0] == ab[3] && ab[1] == ab[2] {
+                    return true;
+                }
+            }
+        }
+
+        false
     }
 }
 
-// Returns a Fibonacci sequence generator
-fn fibonacci() -> Fibonacci {
-    Fibonacci { curr: 0, next: 1 }
-}
+impl FromStr for Ips {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let info: Vec<Info> = s.lines().map(|line| parse(line)).collect();
 
-struct Sample<'a> {
-    s: Vec<&'a i32>,
-}
-
-impl Iterator for Sample<'_> {
-    type Item = i32;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.s.iter().next()
+        Ok(Ips { info })
     }
 }
 
 pub fn run() {
-    // `0..3` is an `Iterator` that generates: 0, 1, and 2.
-    let mut sequence = 0..3;
+    let content = get_file("q7.txt").unwrap();
 
-    println!("Four consecutive `next` calls on 0..3");
-    println!("> {:?}", sequence.next());
-    println!("> {:?}", sequence.next());
-    println!("> {:?}", sequence.next());
-    println!("> {:?}", sequence.next());
-
-    // `for` works through an `Iterator` until it returns `None`.
-    // Each `Some` value is unwrapped and bound to a variable (here, `i`).
-    println!("Iterate through 0..3 using `for`");
-    for i in 0..3 {
-        println!("> {}", i);
-    }
-
-    // The `take(n)` method reduces an `Iterator` to its first `n` terms.
-    println!("The first four terms of the Fibonacci sequence are: ");
-    for i in fibonacci().take(4) {
-        println!("> {}", i);
-    }
-
-    // The `skip(n)` method shortens an `Iterator` by dropping its first `n` terms.
-    println!("The next four terms of the Fibonacci sequence are: ");
-    // for i in fibonacci().skip(4).take(4) {
-    //     println!("> {}", i);
-    // }
-    fibonacci().into_iter().for_each(|n| println!("{n:?}"));
-
-    let array = [1u32, 3, 3, 7];
-
-    // The `iter` method produces an `Iterator` over an array/slice.
-    println!("Iterate the following array {:?}", &array);
-    for i in array.iter() {
-        println!("> {}", i);
-    }
+    let ips: Ips = content.parse().unwrap();
+    println!("{:?}", ips.tls_count());
 }
