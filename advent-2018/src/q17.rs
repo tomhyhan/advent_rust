df349893fcd7ee7040996d24d6dda93d945fbe35
tomhyhan <tomhyhan@gmail.com>
2023-06-14 12:26:35 -0700

    q17

--- advent-2018/src/q17.rs
+++ advent-2018/src/q17.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::{HashMap, VecDeque}, hash::Hash};
 use regex::Regex;
 use advent_2018::{Runner, get_file};
 
@@ -7,16 +7,18 @@ pub struct Q17 {
 }
  
 struct Reservoir {
-    clay_or_water: HashMap<(i32,i32), char>,
+    grid: Vec<Vec<char>>,
     max_y: i32,
+    max_x: i32,
+    min_y: i32,
+    min_x: i32
 }
 
 
 impl Reservoir {
-    fn new() -> Self {
+    fn new() -> Self  {
         let contents = get_file("src/input/q17.txt").unwrap();
-        let mut clay_or_water = HashMap::new();
-        let mut max_y = 0;
+        let mut clays: Vec<(i32, i32)> = vec![];
         contents.lines().for_each(|line| {
             let re = Regex::new(r"\d+").unwrap();
             let nums: Vec<i32>= re.find_iter(line).map(|n| n.as_str().parse::<i32>().unwrap()).collect();
@@ -25,122 +27,135 @@ impl Reservoir {
                 let y_start = nums[1];
                 let y_end = nums[2];
                 for y in y_start..=y_end {
-                    clay_or_water.insert((y,x), '#');
-                    max_y = max_y.max(y);
+                    clays.push((y,x));
                 }
+                
             } else {
                 let y = nums[0];
-                max_y = max_y.max(y);
                 let x_start = nums[1];
                 let x_end = nums[2];
                 for x in x_start..=x_end {
-                    clay_or_water.insert((y,x), '#');
+                    clays.push((y,x));
                 }
             }
         });
-        clay_or_water.insert((0, 500), '|');
-        Self {clay_or_water, max_y} 
+        let mut max_y = i32::MIN;
+        let mut max_x = i32::MIN;
+        let mut min_y = i32::MAX;
+        let mut min_x = i32::MAX;
+        for (y,x) in clays.iter() {
+            max_y = max_y.max(*y);
+            max_x = max_x.max(*x);
+            min_y = min_y.min(*y);
+            min_x = min_x.min(*x);
+        }
+        min_x -= 1;
+        max_x += 1;
+
+        let mut grid: Vec<Vec<char>> = vec![];
+        for i in 0..=max_y {
+            let mut tmp = vec![];
+            for j in 0..(max_x-min_x+1) {
+                tmp.push('.')
+            }
+            grid.push(tmp);
+        }
+        grid[0][(500-min_x) as usize] = '+';
+        for (row,col) in clays.iter() {
+            grid[*row as usize][(col - min_x) as usize] = '#';
+        }
+
+        // for item in grid.iter() {
+        //     println!("{:?}", item.iter().collect::<String>());
+        // }
+        // println!("{:?}", vec!['~','#'].contains(&'.'));
+        Self {grid,max_x,max_y,min_x,min_y}
     }
 
     fn flow(&mut self) {
+        let mut to_visit = VecDeque::from(vec![(1,500)]);
+        
+        while let Some((y,x)) = to_visit.pop_front() {
+            if self.grid[y][(x-self.min_x) as usize] == '.'{
+                self.grid[y][(x-self.min_x) as usize] = '|';
+            } 
+            if y == self.max_y as usize {
+                continue
+            }
+            if self.grid[y+1][(x-self.min_x) as usize] == '.' {
+                to_visit.push_back((y+1,x));
+                continue
+            } else if vec!['~', '#'].contains(&self.grid[y+1][(x-self.min_x) as usize]){
+                if self.grid[y][(x-self.min_x+1) as usize] == '.'{
+                    to_visit.push_back((y,x+1))
+                }
+                if self.grid[y][(x-self.min_x-1) as usize] == '.' {
+                    to_visit.push_back((y,x-1))
+                }
+                if vec!['|', '#'].contains(&self.grid[y][(x-self.min_x+1) as usize]) && vec!['|', '#'].contains(&self.grid[y][(x-self.min_x-1) as usize]) {
+                    let mut right = x;
+                    while vec!['|', '~'].contains(&self.grid[y][(right-self.min_x+1) as usize]) {
+                        right += 1;
+                    }
+                    if self.grid[y][(right-self.min_x+1) as usize] !='#' {
+                        continue
+                    }
+                    let mut left = x;
+                    while vec!['|', '~'].contains(&self.grid[y][(left-self.min_x-1) as usize]) {
+                        left -= 1;
+                    }
+                    if self.grid[y][(left-self.min_x-1) as usize] !='#' {
+                        continue
+                    }
+                    let mut tmp = x;
+                    self.grid[y][(tmp-self.min_x) as usize] = '~';
+                    if self.grid[y-1][(tmp-self.min_x) as usize] == '|'{
+                        to_visit.push_back((y-1,tmp)) 
+                    }
+                    while vec!['|', '~'].contains(&self.grid[y][(tmp-self.min_x+1) as usize]) {
+                        self.grid[y][(tmp-self.min_x+1) as usize] = '~';
+                        tmp += 1;
+                        if self.grid[y-1][(tmp-self.min_x) as usize] == '|' {
+                            to_visit.push_back((y-1,tmp))
+                        }
+                    }
+                    while vec!['|', '~'].contains(&self.grid[y][(tmp-self.min_x-1) as usize]) {
+                        self.grid[y][(tmp-self.min_x-1) as usize] = '~';
+                        tmp -= 1;
+                        if self.grid[y-1][(tmp-self.min_x) as usize] == '|' {
+                            to_visit.push_back((y-1,tmp))
+                        }
+                    }
+                }
+            } 
+        }
         let mut cnt = 0;
-        loop {
-            let current_water = self.clay_or_water.clone();
-            for ((y,x), value) in current_water.iter() {
-                if *value == '|' {
-                    let bottom;
-                    if self.clay_or_water.contains_key(&(y+1, *x)) {
-                        bottom = self.clay_or_water.get(&(y+1, *x)).unwrap();
-                    } else {
-                        bottom = &'.';
-                    }
-    
-                    if *bottom == '#' || *bottom == '~' {
-                        let x_bot_left = self.check_left(y+1, *x, &current_water);
-                        let x_bot_right = self.check_right(y+1, *x, &current_water);
-                        let mut left = None;
-                        let mut right = None;
-                        for left_x in (x_bot_left..*x).rev() {
-                            if let Some(current) = self.clay_or_water.get(&(*y, left_x)) {
-                                if *current == '#' {
-                                    left = Some(left_x);
-                                    break
-                                }
-                            } 
-                        }
-                        for right_x in (*x+1..=x_bot_right) {
-                            if let Some(current) = self.clay_or_water.get(&(*y, right_x)) {
-                                if *current == '#' {
-                                    right = Some(right_x);
-                                    break
-                                }
-                            } 
-                        }
-                        if left.is_some() && right.is_some() {
-                            for river_x in left.unwrap() + 1..right.unwrap() {
-                                self.clay_or_water.insert((*y, river_x), '~');
-                            }
-                        } else if left.is_some() && right.is_none() {
-                            for river_x in left.unwrap()+1..=x_bot_right {
-                                self.clay_or_water.insert((*y, river_x), '|');
-                            }
-                        } else if left.is_none() && right.is_some() {
-                            for river_x in x_bot_left..=right.unwrap() {
-                                self.clay_or_water.insert((*y, river_x), '|');
-                            }
-                        } else if left.is_none() && right.is_none(){
-                            for river_x in x_bot_left..=x_bot_right {
-                                self.clay_or_water.insert((*y, river_x), '|');
-                            }
-                        }
-                    } else {
-                        self.clay_or_water.insert((*y+1, *x), '|');
-                    }
-                }
+        // for item in self.grid.iter() {
+        //     println!("{:?}", item.iter().collect::<String>());
+        //     if cnt == 500 {
+        //         break
+        //     }
+        //     cnt += 1
+        // }
+        // // 27213
+        let mut r = 0;
+        let mut f = 0;
+        println!("{} {}", self.min_y, self.max_y);
+        self.grid[self.min_y as usize..=self.max_y as usize].iter().for_each(|line | line.iter().for_each(|c| {
+            if *c == '~' {
+                r += 1;
             }
-            if cnt == 10000 {
-                break
+            if *c == '|' {
+                f += 1;
             }
-            cnt += 1;
-        }
-
-        for row in 0..self.max_y{
-            let mut s = "".to_string();
-            for col in 450..550 {
-                if self.clay_or_water.contains_key(&(row,col)) {
-                    s.push(*self.clay_or_water.get(&(row,col)).unwrap());
-                } else {
-                    s.push('.');
-                }
-            }
-            println!("{:?}", s);
-        }
-        println!("{:?}", self.clay_or_water.iter().filter(|(_,&v)| v== '~').count());
-        println!("{:?}", self.clay_or_water.iter().filter(|((y,_),&v)| v== '|' && *y <= self.max_y).count() - 1);
+        }));
+        println!("r - {:?}", r);
+        println!("f - {:?}", f);
+        println!("t - {:?}", f + r);
+        println!("{:?}", self.grid.len());
+        println!("{:?}", self.grid[0].len());
     }
 
-    fn check_left(&self, y:i32, x:i32, current_water: &HashMap<(i32,i32), char>) -> i32 {
-        let mut left_x = x;
-        while current_water.contains_key(&(y,left_x)) {
-            let left = current_water.get(&(y,left_x)).unwrap();
-            if left == &'|' {
-                break
-            }
-            left_x -= 1;
-        }
-        left_x
-    }
-    fn check_right(&self,  y:i32, x:i32, current_water: &HashMap<(i32,i32), char>) -> i32 {
-        let mut right_x = x;
-        while current_water.contains_key(&(y,right_x)) {
-            let right = current_water.get(&(y,right_x)).unwrap();
-            if right == &'|' {
-                break
-            }
-            right_x += 1;
-        }
-        right_x
-    }
 }
 
 
