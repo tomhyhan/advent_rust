1199d04f7b79af89fbd150887dd6383b8d3ce332
Tom Han <tomhyhan@gmail.com>
2023-07-04 18:41:16 -0700

    q2

--- advent-2019/src/main.rs
+++ advent-2019/src/main.rs
@@ -1,21 +1,24 @@
 use advent_2019::{copy_file, Runner};
-use std::env;
 use q1::Q1;
 use q2::Q2;
-
+use q3::Q3;
+use std::env;
 
 mod q1;
 mod q2;
+mod q3;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     let mut q1 = Q1::new();
     let mut q2 = Q2::new();
+    let mut q3 = Q3::new();
 
     match args[1].as_str() {
         "cp" => copy_file(args[2].as_str()).unwrap(),
         "1" => q1.run(),
         "2" => q2.run(),
+        "3" => q3.run(),
         _ => panic!("invalid instruction!"),
     }
 }
