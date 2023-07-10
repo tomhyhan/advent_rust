041753ff3268751e56e5cd1f74a0d1b533cd72bd
tomhyhan <tomhyhan@gmail.com>
2023-07-10 13:59:11 -0700

    q9

--- advent-2019/src/main.rs
+++ advent-2019/src/main.rs
@@ -10,6 +10,7 @@ use q5::Q5;
 use q6::Q6;
 use q7::Q7;
 use q8::Q8;
+use q9::Q9;
 
 mod q1;
 mod q2;
@@ -19,6 +20,7 @@ mod q5;
 mod q6;
 mod q7;
 mod q8;
+mod q9;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -31,6 +33,7 @@ fn main() {
     let mut q6 = Q6::new();
     let mut q7 = Q7::new();
     let mut q8 = Q8::new();
+    let mut q9 = Q9::new();
 
     match args[1].as_str() {
         "cp" => copy_file(args[2].as_str()).unwrap(),
@@ -42,6 +45,7 @@ fn main() {
         "6" => q6.run(),
         "7" => q7.run(),
         "8" => q8.run(),
+        "9" => q9.run(),
         _ => panic!("invalid instruction!"),
     }
 }
---
+++ advent-2019/src/q9.rs
@@ -0,0 +1,281 @@
+use std::{collections::{HashMap, HashSet}, f64::consts::PI};
+
+use advent_2019::{Runner, get_file};
+
+pub struct Q9 {
+
+}
+#[derive(Debug)]
+struct Stations {
+    map: HashMap<(i32,i32), char>,
+    max_row: i32,
+    min_row: i32,
+    max_col: i32,
+    min_col: i32,
+}
+
+impl Stations {
+    fn new() -> Self {
+        let content = get_file("src/input/q9.txt").unwrap();
+        let mut max_row = i32::MIN;
+        let mut min_row = i32::MAX;
+        let mut max_col = i32::MIN;
+        let mut min_col = i32::MAX;
+        let mut map = HashMap::new();
+        content.lines().enumerate().for_each(|(row, line)| {
+            line.chars().enumerate().for_each(|(col, char)| {
+                max_row = max_row.max(row as i32); 
+                min_row = min_row.min(row as i32); 
+                max_col = max_col.max(col as i32); 
+                min_col = min_col.min(col as i32); 
+                if char == '#' {
+                    map.insert((row as i32, col as i32), char);
+                }
+            })
+        });
+        Self {map, max_row, min_row, max_col, min_col}
+    }
+
+    fn find_200th_position(&self) {
+        let x = -1.0;
+        let y = 1.0;
+        //  1 1 45 0.7| 1 -1 -45 5.4| -1 -1  -135 3.9| -1 1 135 2.3
+        let angle = f64::atan2(y, x);
+        println!("The angle is: {}", angle);
+        let angle_degrees = f64::to_degrees(angle);
+        println!("The angle in degrees is: {}", angle_degrees);
+        println!("The pi is: {}",  angle );
+        println!("The pi is: {}",  angle + 2.0 * PI);
+    }
+
+    fn find_best_station(&self) -> i32 {
+        let mut max_visible = (0,0,0,HashSet::new());
+        for (row, col) in self.map.keys() {
+            let (num, seen) = self.count_visible_stations(*row, *col);
+            if max_visible.0 < num {
+                max_visible = (num, *row, *col, seen)
+            }
+        }
+        println!("{:?}", max_visible);
+        max_visible.0
+    }
+
+    fn gcd(&self, mut x:i32, mut y:i32) -> i32{
+        if y > x {
+            let tmp = x;
+            x = y;
+            y = tmp;
+        }
+        while y != 0 {
+            let tmp = y;
+            y = x % y;
+            x= tmp
+        }
+        x.abs()
+    }
+
+    fn count_visible_stations(&self, row:i32 ,col:i32) -> (i32,HashSet<(i32,i32)>) {
+        let mut unique_station: HashSet<(i32,i32)> = HashSet::new();
+
+        for (neighbor_row, neighbor_col) in self.map.keys() {
+            if (row, col) == (*neighbor_row , *neighbor_col) {continue;}
+            let diff1 = neighbor_row - row;
+            let diff2 = neighbor_col - col;
+            let gcd = self.gcd(diff1, diff2);
+            let commons_distance = (diff1 / gcd, diff2 / gcd);
+            unique_station.insert(commons_distance);
+        }
+        (unique_station.iter().count() as i32, unique_station)
+    }
+}
+
+impl Q9 {
+    pub fn new() -> Self {
+        Q9 {}
+    }
+
+    fn part1(&mut self) {
+        let stations = Stations::new();
+        stations.find_best_station();
+    }
+
+    fn part2(&mut self) {
+        let stations = Stations::new();
+        stations.find_200th_position();
+    }
+
+}
+
+impl Runner for Q9 {
+    fn part1(&mut self) {
+        self.part1()
+    }
+    
+    fn part2(&mut self) {
+        self.part2()
+    }
+}
+
+
+#[cfg(test)]
+mod test{
+    #[test]
+    fn Q9() {
+        assert_eq!(1, 1);
+    }
+}
+
+
+// #[derive(Debug)]
+// struct Stations {
+//     map: HashMap<(i32,i32), char>,
+//     max_row: i32,
+//     min_row: i32,
+//     max_col: i32,
+//     min_col: i32,
+// }
+
+// impl Stations {
+//     fn new() -> Self {
+//         let content = get_file("src/input/q9.txt").unwrap();
+//         let mut max_row = i32::MIN;
+//         let mut min_row = i32::MAX;
+//         let mut max_col = i32::MIN;
+//         let mut min_col = i32::MAX;
+//         let mut map = HashMap::new();
+//         content.lines().enumerate().for_each(|(row, line)| {
+//             line.chars().enumerate().for_each(|(col, char)| {
+//                 max_row = max_row.max(row as i32); 
+//                 min_row = min_row.min(row as i32); 
+//                 max_col = max_col.max(col as i32); 
+//                 min_col = min_col.min(col as i32); 
+//                 if char == '#' {
+//                     map.insert((row as i32, col as i32), char);
+//                 }
+//             })
+//         });
+//         Self {map, max_row, min_row, max_col, min_col}
+//     }
+
+//     fn find_best_station(&self) -> i32{
+//         let mut max_found = 0;
+
+//         for ((row,col), location) in self.map.iter() {
+//             let found = self.sight_from_current_station(*row, *col);
+//             println!("{:?} {:?}", (row,col), found);
+//             max_found = max_found.max(found);
+//         }
+//         max_found
+//     }
+
+//     fn sight_from_current_station(&self, row: i32, col: i32) -> i32{
+//         let mut min_row = row - 1;
+//         let mut max_row = row + 1;
+//         let mut min_col = col - 1;
+//         let mut max_col = col + 1;
+//         let mut visited = HashSet::new();
+//         let mut found = 0;
+//         while min_row >= self.min_row || max_row <= self.max_row || min_col >= self.min_col || max_col <= self.max_col {
+//             for new_col in min_col..=max_col {
+//                 // println!("{:?}",new_col);
+//                 if self.map.contains_key(&(min_row, new_col)) {
+//                     if visited.contains(&(min_row, new_col)) {
+//                         continue
+//                     }
+//                     visited.insert((min_row, new_col));
+//                     found += 1;
+//                     self.exclude_stations_in_path(min_row, new_col, min_row-row, new_col-col, &mut visited)
+//                 }
+//             }
+
+//             for new_row in min_row+1..=max_row {
+//                 // println!("{:?}",new_row);
+//                 if self.map.contains_key(&(new_row, max_col)) {
+//                     if visited.contains(&(new_row, max_col)) {
+//                         continue
+//                     }
+//                     visited.insert((new_row, max_col));
+//                     found += 1;
+//                     self.exclude_stations_in_path(new_row,max_col, new_row-row, max_col-col, &mut visited)
+//                 }
+//             }
+
+//             for new_col in (min_col..=max_col-1).rev() {
+//                 // println!("{:?}",new_col);
+//                 if self.map.contains_key(&(max_row, new_col)) {
+//                     if visited.contains(&(max_row, new_col)) {
+//                         continue
+//                     }
+//                     visited.insert((max_row, new_col));
+//                     found += 1;
+//                     self.exclude_stations_in_path(max_row, new_col, max_row-row, new_col-col, &mut visited)
+//                 }
+//             }
+
+//             for new_row in (min_row+1..=max_row-1).rev() {
+//                 // println!("{:?}",new_row);
+
+//                 if self.map.contains_key(&(new_row, min_col)) {
+//                     if visited.contains(&(new_row, min_col)) {
+//                     continue
+//                     }
+//                     visited.insert((new_row, min_col));
+//                     found += 1;
+//                     self.exclude_stations_in_path(new_row, min_col, new_row-row, min_col-col, &mut visited)
+//                 }
+//             }
+//             min_row -= 1;
+//             max_row += 1;
+//             min_col -= 1;
+//             max_col += 1;
+//             // println!("{:?}", min_col)
+//             // break
+//         } 
+//         // println!("v f {:?} {:?}", visited, found);
+//         found
+//     }
+
+//     fn cover_everything_in_path(&self, mut row:i32 ,mut col:i32,row_slope: i32, col_slope:i32, visited: &mut HashSet<(i32,i32)>) {
+//         loop  {
+//             row += row_slope;
+//             col += col_slope;
+//             if row < -100 || row > 100 || col < -100 || col > 100 {
+//                 break
+//             }
+//             visited.insert((row,col));
+//         }
+//     }
+
+//     // row >= self.min_row && row <= self.max_row && col >= self.min_col && col <= self.max_col
+//     fn exclude_stations_in_path(&self,crow:i32, ccol:i32, row: i32, col:i32, visited: &mut HashSet<(i32,i32)>) {
+//         // 3 4 , 2 4 => 1, 0
+//         // -1 -1 
+//         //    0 0 
+//         //        1 1
+//         match row.abs() == col.abs(){
+//             true => {
+//                 let row_slope = if row > 0 {1} else {-1};
+//                 let col_slope = if col > 0 {1} else {-1};
+//                 self.cover_everything_in_path(crow, ccol, row_slope, col_slope, visited)
+//             }
+//             false => {
+//                 match (row,col) {
+//                     (0,_) => {
+//                         let row_slope = 0;
+//                         let col_slope = if col > 0 {1} else {-1};
+//                         self.cover_everything_in_path(crow, ccol, row_slope, col_slope, visited)
+//                     },
+//                     (_,0) => {
+//                         let row_slope = if row > 0 {1} else {-1};
+//                         let col_slope = 0;
+//                         self.cover_everything_in_path(crow, ccol, row_slope, col_slope, visited)
+//                     },
+//                     (r,c) => {
+//                         self.cover_everything_in_path(crow, ccol, r, c, visited)
+//                     },
+//                 }
+//             }
+//         }
+//     }
+    
+// }
