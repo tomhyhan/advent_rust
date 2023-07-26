c349459cc8d3cc32868cead765b42a6675b757c0
tomhyhan <tomhyhan@gmail.com>
2023-07-26 11:04:21 -0700

    q20

--- advent-2019/src/q20.rs
+++ advent-2019/src/q20.rs
@@ -1,3 +1,6 @@
+// look for more efficient solution for fun
+// idea => collect all points to travel so instead of going through
+// each block by 1, you go straight to the destination point
 use std::{collections::{HashMap, VecDeque, HashSet}, hash::Hash};
 
 use advent_2019::{Runner, get_file};
@@ -12,7 +15,9 @@ enum Ground {
 struct DonutMaze {
     donut: HashMap<(i32,i32), Ground>,
     outer_transport: HashMap<String,(i32,i32)>,   
+    outer_transport_locations: HashSet<(i32,i32)>,   
     inner_transport: HashMap<String,(i32,i32)>,   
+    inner_transport_locations: HashSet<(i32,i32)>,   
 }
 
 impl DonutMaze {
@@ -56,72 +61,95 @@ impl DonutMaze {
             }
         }
         
-        let outer_transport = get_outer_transport(&outer_sizes, &mut donut, &grid);
-        let inner_transport = get_inner_transport(&mut inner_sizes, &mut donut, &grid);
+        let (outer_transport, mut outer_transport_locations) = get_outer_transport(&outer_sizes, &mut donut, &grid);
+        let (inner_transport,inner_transport_locations) = get_inner_transport(&mut inner_sizes, &mut donut, &grid);
 
-        // println!("{:?}", outer_sizes);
-        // println!("{:?}", inner_sizes);
-        // println!("{:?}", outer_transport);
-        // println!("{:?}", inner_transport);
-        // println!("{:?}", donut);
+        let start = outer_transport.get("AA").unwrap();
+        let end = outer_transport.get("ZZ").unwrap();
+        outer_transport_locations.remove(start);
+        outer_transport_locations.remove(end);
 
-        Self {donut,inner_transport,outer_transport}
+        println!("{:?}", inner_transport);
+        println!("{:?}", inner_transport_locations);
+        Self {donut,inner_transport,outer_transport,inner_transport_locations,outer_transport_locations}
     }
 
     fn traverse_maze(&self) {
         let start = *self.outer_transport.get("AA").unwrap();
-        // let end = *self.outer_transport.get("ZZ").unwrap();
+        let end = *self.outer_transport.get("ZZ").unwrap();
         let mut queue = HashSet::from([(start, 0, vec![],0)]);
         let mut visited = HashSet::new();
-        'outer: loop {
-            let current = queue.iter().min_by(|x,y|(x.3 as i32).abs().cmp(&(y.3 as i32).abs())).unwrap().clone();
-            
+        loop {
+            let current = queue.iter().min_by(|x,y|x.1.cmp(&y.1)).unwrap().clone();
+            // let current = queue.iter().min_by(|x,y|(x.3 as i32).abs().cmp(&(y.3 as i32).abs())).unwrap().clone();
             if !queue.remove(&current) {
                 panic!("it should be removed!")
             }
-
             let  ((row,col), distance, path, level) = current;
+            // println!("{row} {col} {distance}");
+            // println!("{distance}");
             if !visited.insert((row,col, level)) {
                 continue
             }   
+            // println!("{:?}", queue);
+            if level == 0 && (row,col) == end {
+                println!("distance - {distance}");
+                println!("path - {path:?}");
+                break
+            }
 
-            for dir in [(-1,0),(1,0),(0,-1),(0,1)] {
+            for dir in [(0,1),(0,-1),(1,0),(-1,0)] {
                 let (nrow, ncol) = (row + dir.0, col + dir.1);
                 let next = self.donut.get(&(nrow,ncol)).unwrap();
-
+                // println!("{:?}", next);
                 match next {
-                    Ground::Open => {queue.insert(((nrow,ncol), distance +1,path.clone(), level));},
+                    Ground::Open=> {
+                        queue.insert(((nrow,ncol), distance +1,path.clone(), level));},
                     Ground::Transport(letter) => {
-                        let mut new_path = path.clone();
-                        new_path.push(letter);
-                        if level != 0 && (letter == "AA" || letter == "ZZ") {
-                            continue
-                        } else if letter == "ZZ" {
-                            println!("{distance} {path:?}");
-                            break 'outer;
-                        } else if letter =="AA" {
+                        if letter == "AA"  {
+                            continue;
+                        };
+                        if level == 0 &&  letter == "ZZ" {
+                            let mut np = path.clone();
+                            np.push(letter);
+                            queue.insert(((nrow,ncol), distance +1,np, level));
                             continue
                         }
-                        // println!("{letter} {level}");
-                        let outer = self.outer_transport.get(letter).unwrap();
-                        let inner = self.inner_transport.get(letter).unwrap();
-
-                        if (nrow,ncol) == *outer {
-                            queue.insert((*inner, distance +2, new_path, level - 1));
-                        } else {
-                            queue.insert((*outer, distance +2,new_path, level + 1));
-                        }
-                    },
+                        queue.insert(((nrow,ncol), distance +1,path.clone(), level));
+                    }
                     _ => continue
                 }
             }
+            
+            let ground = self.donut.get(&(row,col)).unwrap();
+            // println!("ground - {:?}", ground);
+            if let Ground::Transport(letter) = ground {
+                // println!("{:?}", letter);
+                let mut new_path = path.clone();
+                new_path.push(letter);
+    
+                if self.outer_transport_locations.contains(&(row,col)) {
+                    if level -1 < 0 {
+                        continue
+                    }
+                    let inner = self.inner_transport.get(letter).unwrap();
+                    queue.insert((*inner, distance +1, new_path, level - 1));
+                } else if self.inner_transport_locations.contains(&(row,col)){
+                    let outer = self.outer_transport.get(letter).unwrap();
+                    
+                    queue.insert((*outer, distance +1,new_path, level + 1));
+                }
+
+            }
+            
         }
         // print!("{:?}", self.outer_transport);
     }
 }
 
-fn get_outer_transport(outer_sizes: &[i32;4], donut: &mut HashMap<(i32,i32), Ground>, grid: &Vec<Vec<char>>) -> HashMap<String, (i32, i32)>{
+fn get_outer_transport(outer_sizes: &[i32;4], donut: &mut HashMap<(i32,i32), Ground>, grid: &Vec<Vec<char>>) -> (HashMap<String, (i32, i32)>, HashSet<(i32,i32)>){
     let mut outer_transport = HashMap::new();
+    let mut outer_transport_locations = HashSet::new();
     for row in outer_sizes[0]..=outer_sizes[1] {
         for col in outer_sizes[2]..=outer_sizes[3] {
             let current = donut.get(&(row,col)).unwrap();
@@ -137,13 +165,15 @@ fn get_outer_transport(outer_sizes: &[i32;4], donut: &mut HashMap<(i32,i32), Gro
             } else {continue}
             outer_transport.insert(letter.clone(), (row,col));
             donut.insert((row,col), Ground::Transport(letter));
+            outer_transport_locations.insert((row,col));
         }
     }
-    outer_transport
+    (outer_transport, outer_transport_locations)
 }
 
-fn get_inner_transport(inner_sizes: &mut [i32;4], donut: &mut HashMap<(i32,i32), Ground>, grid: &Vec<Vec<char>>) -> HashMap<String, (i32, i32)>{
+fn get_inner_transport(inner_sizes: &mut [i32;4], donut: &mut HashMap<(i32,i32), Ground>, grid: &Vec<Vec<char>>) -> (HashMap<String, (i32, i32)>, HashSet<(i32,i32)>){
     let mut inner_transport = HashMap::new();
+    let mut inner_transport_locations  = HashSet::new();
     inner_sizes[0] -=1;
     inner_sizes[1] +=1;
     inner_sizes[2] -=1;
@@ -163,9 +193,10 @@ fn get_inner_transport(inner_sizes: &mut [i32;4], donut: &mut HashMap<(i32,i32),
                 } else {continue}
             inner_transport.insert(letter.clone(), (row,col));
             donut.insert((row,col), Ground::Transport(letter));
+            inner_transport_locations.insert((row,col));
         }
     }
-    inner_transport
+    (inner_transport, inner_transport_locations)
 }
 pub struct Q20 {
 
@@ -205,7 +236,79 @@ mod test{
         assert_eq!(1, 1);
     }
 }
+// part2
+// let start = *self.outer_transport.get("AA").unwrap();
+//         let end = *self.outer_transport.get("ZZ").unwrap();
+//         let mut queue = VecDeque::from([(start, 0, vec![],0)]);
+//         let mut visited = HashSet::new();
+//         println!("{:?}", start);
+//         while let Some(((row,col), distance, path, level)) = queue.pop_front(){
+//             // let current = queue.iter().min_by(|x,y|x.1.cmp(&y.1)).unwrap().clone();
+//             // let current = queue.iter().min_by(|x,y|(x.3 as i32).abs().cmp(&(y.3 as i32).abs())).unwrap().clone();
+//             // if !queue.remove(&current) {
+//             //     panic!("it should be removed!")
+//             // }
+//             // let  ((row,col), distance, path, level) = current;
+//             // println!("{row} {col} {distance}");
+//             // println!("{distance}");
+//             if !visited.insert((row,col, level)) {
+//                 continue
+//             }   
+//             // println!("{:?}", queue);
+//             if level == 0 && (row,col) == end {
+//                 println!("distance - {distance}");
+//                 println!("path - {path:?}");
+//                 break
+//             }
 
+//             for dir in [(0,1),(0,-1),(1,0),(-1,0)] {
+//                 let (nrow, ncol) = (row + dir.0, col + dir.1);
+//                 let next = self.donut.get(&(nrow,ncol)).unwrap();
+//                 // println!("{:?}", next);
+//                 match next {
+//                     Ground::Open=> {
+//                         queue.push_back(((nrow,ncol), distance +1,path.clone(), level));},
+//                     Ground::Transport(letter) => {
+//                         if letter == "AA"  {
+//                             continue;
+//                         };
+//                         if level == 0 &&  letter == "ZZ" {
+//                             let mut np = path.clone();
+//                             np.push(letter);
+//                             queue.push_back(((nrow,ncol), distance +1,np, level));
+//                             continue
+//                         }
+//                         queue.push_back(((nrow,ncol), distance +1,path.clone(), level));
+//                     }
+//                     _ => continue
+//                 }
+//             }
+            
+//             let ground = self.donut.get(&(row,col)).unwrap();
+//             // println!("ground - {:?}", ground);
+//             if let Ground::Transport(letter) = ground {
+//                 // println!("{:?}", letter);
+//                 let mut new_path = path.clone();
+//                 new_path.push(letter);
+    
+//                 if self.outer_transport_locations.contains(&(row,col)) {
+//                     if level -1 < 0 {
+//                         continue
+//                     }
+//                     let inner = self.inner_transport.get(letter).unwrap();
+//                     queue.push_back((*inner, distance +1, new_path, level - 1));
+//                 } else if self.inner_transport_locations.contains(&(row,col)){
+//                     let outer = self.outer_transport.get(letter).unwrap();
+                    
+//                     queue.push_back((*outer, distance +1,new_path, level + 1));
+//                 }
+
+//             }
+            
+//         }
+//         // print!("{:?}", self.outer_transport);
+//     }
+// part1
 // fn traverse_maze(&self) {
 //     let start = *self.outer_transport.get("AA").unwrap();
 //     let end = *self.outer_transport.get("ZZ").unwrap();
