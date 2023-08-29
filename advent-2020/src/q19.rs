f877521368eb3745ecf9797de62bf369b418e542
tomhyhan <tomhyhan@gmail.com>
2023-08-29 14:58:48 -0700

    q19

--- advent-2020/src/q19.rs
+++ advent-2020/src/q19.rs
@@ -21,9 +21,21 @@ impl Satellite {
 
     fn valid_messages(&self, start: String) {
         let matches = self.rules.get(&start).unwrap();
+        let mut comb = vec![vec!["".to_string()]];
         for m in matches.split_whitespace() {
-            println!("{:?}", self.get_messages(m.to_string()));
+            let current_comb = self.get_messages(m.to_string());
+            comb.push(current_comb);
+            let c = comb.clone();
+            comb = vec![self.combine_vec(c)]; 
         }
+        println!("{:?}", comb);
+        let mut valids = 0;
+        for c in comb[0].iter() {
+            if self.valids.contains(c) {
+                valids += 1;
+            }
+        }
+        println!("valids - {:?}", valids);
     }
 
     fn get_messages(&self, rule: String) -> Vec<String>{
@@ -37,15 +49,27 @@ impl Satellite {
         for sub_rules in matches.split(" | ") {
             let mut sub_message = vec![];
             for m in sub_rules.split_whitespace() {
-                sub_message.extend(self.get_messages(m.to_string()));
+                sub_message.push(self.get_messages(m.to_string()));
             }
-            for i in sub_message[1].chars() {
-
-                // messages.push();
-            }
+            // println!("sub - {:?} {}", sub_message, rule);
+            let comb = self.combine_vec(sub_message);
+            messages.extend(comb);
         }
         // println!("messages - {:?}", messages);
         messages
+    }
+
+    fn combine_vec(&self, vec: Vec<Vec<String>>) -> Vec<String>{
+        let mut comb = vec![];
+        if vec.len() == 1 {
+            return vec[0].clone()
+        }
+        for v1 in vec[0].iter() {
+            for v2 in vec[1].iter() {
+                comb.push(format!("{v1}{v2}"));
+            }
+        }
+        comb
     }
 }
 
@@ -62,11 +86,13 @@ impl Q19 {
 
     fn part1(&mut self) {
         let satellite = Satellite::new();
-        satellite.valid_messages("0".to_string());
+        // satellite.valid_messages("0".to_string());
     }
 
     fn part2(&mut self) {
-        
+        let satellite = Satellite::new();
+        println!("{:?}", satellite.get_messages("42".to_string()));
+        println!("{:?}", satellite.get_messages("31".to_string()));
     }
 
 }
