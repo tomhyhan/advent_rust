f2302427abc124841f0545a65069d41a7c29f86e
tomhyhan <tomhyhan@gmail.com>
2023-09-01 16:00:33 -0700

    q20

--- advent-2020/src/q20.rs
+++ advent-2020/src/q20.rs
@@ -3,8 +3,70 @@ use regex::Regex;
 use lazy_static::lazy_static;
 use advent_2020::{Runner, get_file};
 
+
+#[derive(Debug, Default, Clone)]
+struct Image {
+    tile: Vec<Vec<char>>
+    
+}
+
+impl Image {
+    fn new(tile: Vec<Vec<char>>) -> Self  {
+        Self {tile}
+    }
+
+    fn rotate(&mut self) {
+        for i in 0..self.tile.len() {
+            for j in i+1..self.tile[0].len() {
+                let temp = self.tile[i][j];
+                self.tile[i][j] = self.tile[j][i];
+                self.tile[j][i] = temp;
+            }
+        }
+        self.flip();
+    }
+
+    fn flip(&mut self) {
+        for tile in self.tile.iter_mut() {
+            tile.reverse();
+        }
+    }
+
+    fn rotations(&self) -> Vec<Image>{
+        let mut rotations = vec![]; 
+        let mut tile = self.clone();    
+        for _ in 0..4 {
+            rotations.push(tile.clone());
+            tile.rotate()
+        }
+        tile.flip();
+        for _ in 0..4 {
+            rotations.push(tile.clone());
+            tile.rotate()
+        }
+        rotations
+    }
+
+    fn top(&self) -> Vec<char> {
+        self.tile[0].clone()
+    }
+
+    fn bottom(&self) -> Vec<char> {
+        self.tile[self.tile.len()-1].clone()
+    }
+
+    fn left(&self) -> Vec<char> {
+        self.tile.iter().map(|tile|tile[0]).collect()
+    }
+
+    fn right(&self) -> Vec<char> {
+        self.tile.iter().map(|tile|tile[tile.len()-1]).collect()
+    }
+
+}
+
 struct CameraArray {
-    images: HashMap<i64, Vec<Vec<char>>>
+    images: HashMap<i64, Image>
 }
 
 impl CameraArray {
@@ -20,124 +82,77 @@ impl CameraArray {
             let first_line = tile_lines.next().unwrap();
             let num = RE.find(first_line).unwrap().as_str();
             let id = num.parse::<i64>().unwrap();
-            let image: Vec<Vec<char>>= tile_lines.map(| line| line.chars().collect()).collect();
-            images.insert(id, image);
+            let tile: Vec<Vec<char>>= tile_lines.map(| line| line.chars().collect()).collect();
+            images.insert(id, Image { tile });
         });
         Self { images }
     }
 
-    fn rotate(&self, image: &mut Vec<Vec<char>>) {
-        image.reverse()
+    fn images(&mut self) {
+        let len = (self.images.len() as f64).sqrt() as usize;
+        let mut tiles: Vec<Vec<Image>> = vec![vec![Image::default();len];len];
+        let mut tiles_ids: Vec<Vec<_>> = vec![vec![0;len];len];
+        let mut seen = HashSet::new(); 
+        self.fill_tiles(0,0, &mut tiles, &mut seen, len, &mut tiles_ids);
+        // println!("{:?}", tiles);
+        println!("{:?}", tiles_ids[0][0] * tiles_ids[0][len-1] * tiles_ids[len-1][0] * tiles_ids[len-1][len-1]);
     }
 
-    fn flip(&self, image: &mut Vec<Vec<char>>) {
-        for tile in image.iter_mut() {
-            tile.reverse();
+    fn check_left(&self, current: &Image, other: &Image) -> bool {
+        if current.left() == other.right() {
+            return true
         }
-    }
-
-    fn match_y(&self, side: &Vec<char>, id: &i64) -> bool {
-        for (cid, image) in self.images.iter() {
-            if id == cid {
-                continue
-            }
-            let rotations = self.generate_4sides(image.clone());
-            for rotation in rotations {
-                if side == &rotation[9] {
-                    return true
-                }
-            }
-        }            
         false
     }
 
-    fn match_left(&self, side: &Vec<char>, seen: &mut HashSet<i64>) -> bool {
-        for (cid, image) in self.images.iter() {
-            if seen.contains(cid) {
-                continue
-            }
-            let rotations = self.generate_4sides(image.clone());
-            for rotation in rotations {
-                let left: Vec<_> = rotation.iter().map(|line|line[0]).collect();
-                if side == &left {
-                    return true
-                }
-            }
-        }            
+    fn check_top(&self, current: &Image, other: &Image) -> bool {
+        if current.top() == other.bottom() {
+            return true
+        }
         false
     }
 
-    fn generate_4sides(&self, mut image: Vec<Vec<char>>) -> Vec<Vec<Vec<char>>> {
-        let mut rotations = vec![];    
-        for i in 0..4 {
-            if i % 2 == 0 {
-                self.flip(&mut image);      
-            } else {
-                self.rotate(&mut image)      
-            }
-            rotations.push(image.clone());
+    fn fill_tiles(&self, row: usize, col: usize, tiles: &mut Vec<Vec<Image>>, seen: &mut HashSet<i64>, len: usize, tiles_ids: &mut Vec<Vec<i64>>) -> bool{
+        
+        if row >= len || col >= len {
+            return true
         }
-        rotations
-    }
-
-    // fn top_left(&self) {
-    //     for (id, image) in self.images.iter() {
-    //         let rotations = self.generate_4sides(image.clone());
-    //         for rotation in rotations {
-    //             let top = &rotation[0];
-    //             let left: Vec<_> = rotation.iter().map(|line|line[0]).collect();
-    //             if self.match_y(top,id) {
-    //                 continue
-    //             }
-    //             if self.match_x(&left,id) {
-    //                 continue
-    //             }
-    //             println!("id - {:?}", id);
-    //             return
-    //         }
-    //     }
-    // }
-
-    fn create_image(&self, mut row: usize, mut col: usize, board: &mut Vec<Vec<Vec<Vec<char>>>>, seen: &mut HashSet<i64>) {
-        if seen.len() == 9 {
-            println!("{:?}", board);
-            return
-        }
-        if row == 2 && col == 3 {
-            println!("{:?}", board)
-        }
-        if col > 3 {
-            row += 1;
-            col = 0
-        }
-
+        
         for (id, image) in self.images.iter() {
-            if seen.contains(id) {
-                continue
+            if !seen.insert(*id) {
+                continue;
             }
-            let rotations = self.generate_4sides(image.clone());
-            for rotation in rotations {
-                match (row,col) {
-                    (0,0) => {
-                        board[row][col] = rotation.clone();
-                        seen.insert(*id);
-                        self.create_image(row, col+1, board, seen);
-                        seen.remove(id);
-                        board[row][col].clear()
+            // println!("{}, {}", row,col);
+            for rotation in image.rotations() {
+                
+                tiles[row][col] = rotation.clone();
+                tiles_ids[row][col] = *id;
+                let mut left = true;
+                let mut top = true;
+                
+                if row > 0 {
+                    top = self.check_top(&rotation, &tiles[row-1][col]);
+                }
+    
+                if col > 0 {
+                    left = self.check_left(&rotation, &tiles[row][col-1])
+                }
+    
+                if left && top {
+                    let nc = (col + 1) % len;
+                    // let nr = ((col+1)/len) + row;
+                    let nr = (len * row + col + 1) / len;
+                    if self.fill_tiles(nr, nc, tiles, seen, len, tiles_ids) {
+                        return true
                     }
-                    (0,_) => {
-                        let prev = &board[row][col-1];
-                        
-                    }
-                    (_,0) => {
-
-                    }
-                    (_,_) => {
-                        
-                    }
-                }                 
+                }
+                tiles[row][col] = Image::default();
+                tiles_ids[row][col] = -1;
             }
+            seen.remove(id);
         }
+
+        false
     }
 }
 
@@ -151,9 +166,8 @@ impl Q20 {
     }
 
     fn part1(&mut self) {
-        let camera = CameraArray::new();
-        // println!("{:?}", camera.images.len())
-        // camera.top_left()
+        let mut camera = CameraArray::new();
+        camera.images();
     }
 
     fn part2(&mut self) {
@@ -175,8 +189,15 @@ impl Runner for Q20 {
 
 #[cfg(test)]
 mod test{
+    use super::*;
     #[test]
     fn Q20() {
-        assert_eq!(1, 1);
+        let mut image = Image::new(vec![vec!['1','2','3'],vec!['4','5','6'],vec!['7','8','9']]);
+        // image.rotate();
+        // for t in image.tile {
+        //     println!("{:?}", t);
+        // };
+        println!("{:?}", image.top());
+        assert_eq!(1, 2);
     }
 }
