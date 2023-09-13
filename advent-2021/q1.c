7c4e54bf8de6384cb8d30b7ffc29b4f7e2a64c9e
tomhyhan <tomhyhan@gmail.com>
2023-09-13 09:45:58 -0700

    q1

--- advent-2021/q1.c
+++ advent-2021/q1.c
@@ -9,11 +9,8 @@ int main(void)
     errno_t err;
     
     char line[N];
-    int nums[N];
-    int num;
-    int length = sizeof(nums) / sizeof(nums[0]);
+    int nums[N], num, i, previous, current;
     int count = 0, matches = 0;
-    int i, previous;
 
     err = fopen_s(&file, "./inputs/q1.txt", "r");
     if (err != 0) {
@@ -30,14 +27,15 @@ int main(void)
         }
     }
 
-    previous = nums[0];
-    for (i = 1; i < length; i++) {
-        if (nums[i] > previous) {
+    previous = nums[0] + nums[1] + nums[2];
+    for (i = 1; i < count-2; i++) {
+        current = nums[i] + nums[i+1] + nums[i+2]; 
+        if (current > previous) {
             matches++;
         }
-        previous = nums[i];
+        previous = current;
     }
-    printf("%d", matches);
+    printf("matches - %d", matches);
 
     fclose(file);
 
