0bb440fdaea6dbe2a8ffcaf5b6cdf652af9d8758
tomhyhan <tomhyhan@gmail.com>
2023-11-27 11:08:19 -0800

    q19-1

--- advent-2021/q19-1.py
+++ advent-2021/q19-1.py
@@ -60,18 +60,35 @@ def max_coord_distance(point1, point2):
 
 def manhattan_distance(point1, point2):
     diff = point1 - point2
-    return max(abs(diff.x) + abs(diff.y) + abs(diff.z))
+    return abs(diff.x) + abs(diff.y) + abs(diff.z)
 
 def fingerprint(point1, point2):
     return (manhattan_distance(point1, point2) + max_coord_distance(point1, point2))
 
 def find_match(known_beacons, known_fingerprints, report, report_fingerprints):
-    pass
+    matching_fgp = [(fgp, report_pair) for (fgp, report_pair) in report_fingerprints if fgp in known_fingerprints]
+
+    for fgp, report_pair in matching_fgp:
+        for pair in known_fingerprints[fgp]:
+            p1, p2 = pair
+            rp1, rp2 = report_pair
+            
+            for rotation in rotations:
+                offset = p1 - rotation(rp1)
+                if offset != p2 - rotation(rp2):
+                    continue
+                transforms = [rotation(p) + offset for p in report]
+                relatives = 0
+                for transform in transforms:
+                    if transform in known_beacons:
+                        relatives += 1
+                    if relatives >= 12:
+                        return (offset, transforms)
 
 
 def map_beacons(reports):
-    current_beacons = reports.pop(0)
-    fingerprints = defaultdict()
+    current_beacons = set(reports.pop(0))
+    fingerprints = defaultdict(list)
     for p1, p2 in combinations(current_beacons, 2):
         fingerprints[fingerprint(p1,p2)].append((p1,p2))  
     
@@ -79,17 +96,36 @@ def map_beacons(reports):
     
     possible_fingerprints = []
     for report in reports:
-        pass
-    
-    pass
+        fgs = [(fingerprint(p1,p2), (p1,p2)) for p1, p2 in combinations(report, 2)]  
+        possible_fingerprints.append([report, fgs])
 
-
+    while possible_fingerprints:
+        for idx, (report, fgs) in enumerate(possible_fingerprints):
+            if (result := find_match(current_beacons, fingerprints, report ,fgs)):
+                yield result                
+                _, transformed_report = result
+                for p1, p2 in combinations(transformed_report,2):
+                    fingerprints[fingerprint(p1,p2)].append((p1, p2))
+                current_beacons.update(transformed_report)
+                del possible_fingerprints[idx]
+                break
+            
 def part1():
     input = open("./inputs/q19.txt").read()
     scanners = parse_scanner_reports(input)
-    print(scanners)
-    _ , beacons = map_beacons(scanners)
-
+    beacons_map = set()
+    for (_, beacons) in map_beacons(scanners):
+        for beacon in beacons:
+            beacons_map.add(beacon)
+    print(len(beacons_map))
+    
+    # x = list(p for p in combinations([(0,2), (4,1), (3,3)], 2))
+    # y = list(p for p in combinations([(-1,-1), (-5,0), (-2,1)], 2))
+    # for p1, p2 in x:
+    #     for r1, r2 in y:
+    #         print(p1,p2,r1,r2)
+    #         print(p1[0] - r1[0], p2[1] - r2[1])
+    #         print()
 def parse_scanner_reports(input):
     return [[Point3D(*map(int, beacon.split(','))) for beacon in scanners.split('\n') if beacon[1] != '-'] for scanners in input.split("\n\n") ]
 
