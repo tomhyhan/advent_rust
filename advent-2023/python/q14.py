45379fa9905ac51e279b20c05907182f97b11939
tomhyhan <tomhyhan@gmail.com>
2023-12-14 15:43:18 -0800

    q14

--- advent-2023/python/q14.py
+++ advent-2023/python/q14.py
@@ -1,8 +1,23 @@
-def part1():
-    pass
+def part1(lines):
+    vertical_map = transpos(lines)
+    print(vertical_map)
+    moved = True
+    while moved: 
+        moved = False
+        for line in vertical_map:
+            for i in range(len(line) - 1):
+                if line[i] == '.' and line[i+1] == 'O': 
+                    line[i], line[i+1] = line[i+1], line[i]
+                    moved = True
+    print(vertical_map)
+    
+def transpos(lines):
+    return [zip(*lines)]
 
 def solution():
     filename = "./inputs/q14.txt"
-    pass
+    lines = [list(line) for line in open(filename).read().split('\n')]
+    part1(lines)
+
 
 solution()
