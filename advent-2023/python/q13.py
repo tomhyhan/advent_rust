f57db780f00588bf24ec144f6d8265b337ee1337
tomhyhan <tomhyhan@gmail.com>
2023-12-14 12:06:39 -0800

    q14

--- advent-2023/python/q13.py
+++ advent-2023/python/q13.py
@@ -7,7 +7,6 @@ def parse(filename):
         cols = [''.join(map(lambda line: line[col], lines)) for col in range(len(lines[0]))]
         ps.append([rows, cols])
     return ps
-    pass
 
 def is_match(pattern, s):
     left, right = s - 1, s 
@@ -18,26 +17,60 @@ def is_match(pattern, s):
         right += 1
     return True
 
-def does_reflect(pattern):
+def does_reflect(pattern, part2, original = 0):
     for i in range(1, len(pattern)):
         if is_match(pattern, i):
+            if part2 and i == original:
+                continue
             return i
     return False
 
 def part1(patterns):
     total = 0
-    for pattern in patterns:
+    mirror_pos = {}
+    for idx, pattern in enumerate(patterns):
         rows, cols = pattern
-        mirror = does_reflect(rows)
+        mirror = does_reflect(rows, False)
         if mirror:
             total += mirror * 100
+            mirror_pos[idx] = mirror * 100
         else:
-            total += does_reflect(cols)
+            mirror = does_reflect(cols, False)
+            total += mirror
+            mirror_pos[idx] = mirror 
     print(total)    
+    return mirror_pos
+
+def check_smudge(current, mirror_score, is_col):
+    for row in range(len(current)):
+        for col in range(len(current[row])):
+            temp = current[row][col] 
+            current[row][col] = '.' if current[row][col] == '#' else '#' 
+            original = mirror_score if is_col else mirror_score // 100
+            if mirror:= does_reflect(current, True, original):
+                mirror = mirror if is_col else mirror * 100
+                return mirror
+                    
+            current[row][col] = temp
+    return False
+    
+def part2(mirror_pos, patterns):
+    total = 0
+    for idx, pattern in enumerate(patterns):
+        mirror_score = mirror_pos[idx]
+        for is_col, current in enumerate(pattern):
+            mirror = check_smudge([list(line) for line in current], mirror_score, is_col)
+            if mirror:
+                total += mirror
+                break
+    print(total)
 
 def solution():
+    # improve with xor, and diff == 1
+    # new_trick: list(zip(*test.split('\n')))
     filename = "./inputs/q13.txt"
     patterns = parse(filename)
-    part1(patterns)
-    
+    mirror_pos = part1(patterns)
+    part2(mirror_pos, patterns)
+
 solution()
---
+++ advent-2023/python/q14.py
@@ -0,0 +1,8 @@
+def part1():
+    pass
+
+def solution():
+    filename = "./inputs/q14.txt"
+    pass
+
+solution()
