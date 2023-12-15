3d2a2d4baf871ab59efa50e34b85f2bd6aa30f0c
Tom Han <tomhyhan@gmail.com>
2023-12-14 22:32:18 -0800

    q14

--- advent-2023/python/q14.py
+++ advent-2023/python/q14.py
@@ -2,17 +2,24 @@ def part1(lines):
     vertical_map = transpos(lines)
     print(vertical_map)
     moved = True
-    while moved: 
+    while moved:
         moved = False
         for line in vertical_map:
             for i in range(len(line) - 1):
-                if line[i] == '.' and line[i+1] == 'O': 
+                if line[i] == '.' and line[i+1] == 'O':
                     line[i], line[i+1] = line[i+1], line[i]
                     moved = True
-    print(vertical_map)
-    
+    horizontal_map = transpos(vertical_map)
+    length = len(horizontal_map)
+    for idx, r in enumerate(horizontal_map):
+        print(''.join(r))
+
+    # print(vertical_map)
+
+
 def transpos(lines):
-    return [zip(*lines)]
+    return [list(row) for row in zip(*lines)]
+
 
 def solution():
     filename = "./inputs/q14.txt"
