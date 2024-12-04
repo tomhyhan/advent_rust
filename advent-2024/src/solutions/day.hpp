d62dac631c733db08aefac1270d1a96b40a72046
tomhyhan <tomhyhan@gmail.com>
2024-12-04 11:40:41 -0800

    day4

--- advent-2024/src/solutions/day.hpp
+++ advent-2024/src/solutions/day.hpp
@@ -12,6 +12,8 @@
 #include <unordered_map>
 #include <ranges>
 
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+
 using namespace std;
 
 class Day {
@@ -47,5 +49,5 @@ class Day4 : public Day {
         string solve(stringstream& input_buffer) override;
     protected:
         void part1(vector<string>& grid);
-        void part2(stringstream& input_buffer);
+        void part2(vector<string>& grid);
 };
--- advent-2024/src/solutions/day4.cpp
+++ advent-2024/src/solutions/day4.cpp
@@ -1,6 +1,10 @@
 #include "day.hpp"
 
 const string XMAS = "XMAS";
+const string X_MAS = "MAS";
+
+const int DIRECTIONS[4][2] = {{0,2},{2,0},{0,-2},{-2,0}};
+const int MOVES[4][2][2][2] = {{{{1,1},{1,-1}},{{-1,1},{-1,-1}}}, {{{1,1},{-1,1}},{{1,-1},{-1,-1}}},{{{1,-1},{1,1}},{{-1,-1},{-1,1}}},{{{-1,1},{1,1}},{{-1,-1},{1,-1}}}};
 
 string Day4::solve(stringstream& input_buffer) {
     vector<string> grid;
@@ -10,6 +14,7 @@ string Day4::solve(stringstream& input_buffer) {
         grid.push_back(line);
     }
     part1(grid);
+    part2(grid);
     return "day4";
 }
 
@@ -65,10 +70,50 @@ void Day4::part1(vector<string>& grid) {
     cout << "Part1: " << n_xmas << "\n";
 }
 
-void Day4::part2(stringstream& input_buffer) {
-
+bool check_condition(int row, int col, int idx, vector<string>& grid) {
+    if (row >=0 && row < grid.size() && col >= 0 && col < grid[0].size() && grid[row][col] == X_MAS[idx]) {
+        return true;
+    }
+    return false;
 }
-// if (visited.contains({row, col}) || idx >= 3) {
-//     continue;
-// }
-// visited.insert({row, col});
+
+// 1. instant match using brute force
+// 2. sperately create and match diagnol "MAS" pairs
+void Day4::part2(vector<string>& grid) {
+
+    int n_xmas = 0;
+    for (int row=0; row < grid.size(); ++row) {
+        for (int col=0; col < grid[0].size(); ++col) {
+            if (grid[row][col] == 'M') {
+                for (int i=0; i < ARRAY_SIZE(DIRECTIONS); i++) {
+                    auto [rmov, cmov] = DIRECTIONS[i];
+                    int crow = row;
+                    int ccol = col;
+                    int nrow = crow + rmov;
+                    int ncol = ccol + cmov;
+ 
+                    for (int j=0; j < 2; j++) {
+                        const auto [first_pair, second_pair] = MOVES[i][j];
+                        const auto [drmov, dcmov] = first_pair;
+                        const auto [dnrmov, dncmov] = second_pair;
+ 
+                        for (int idx = 0; idx < 3; idx++) {
+                            int r = crow + drmov * idx;
+                            int c = ccol + dcmov * idx;
+                            int nr = nrow + dnrmov * idx;
+                            int nc = ncol + dncmov * idx;
+                            if (!(check_condition(r, c, idx, grid) && check_condition(nr, nc, idx, grid))) {
+                                break;
+                            }
+                            if (idx == 2) {
+                                n_xmas += 1;
+                            }
+                        }
+                    }                    
+                }
+            }
+        }
+    }
+    // 1096 X
+    cout << "Part2: " << (n_xmas / 2) << "\n";
+}
