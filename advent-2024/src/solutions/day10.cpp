4ce8d10f900e3ea0efd8f8a1d8a416353353f1a8
tomhyhan <tomhyhan@gmail.com>
2024-12-11 09:12:07 -0800

    day10

--- advent-2024/src/solutions/day10.cpp
+++ advent-2024/src/solutions/day10.cpp
@@ -7,7 +7,7 @@ function<int(char)> ctoi = [](char a) {return a - '0';};
 const int DIRECTIONS[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
 
 void part1(vector<vector<int>> grid);
-
+void part2(vector<vector<int>> grid);
 
 string Day10::solve(stringstream& input_buffer) {
     vector<vector<int>> grid;
@@ -22,11 +22,38 @@ string Day10::solve(stringstream& input_buffer) {
     }
 
     part1(grid);
+    part2(grid);
     
     return "day10";
 }
 
-int count_routes(int row, int col, vector<vector<int>> grid, map<Point_t, int> scores) {
+set<int> count_routes(int row, int col, vector<vector<int>> grid, map<Point_t, set<int>> scores) {
+    Point_t key = {row,col};
+    auto score = scores.find(key);
+    int n_col = grid[0].size();
+
+    if (score != scores.end()) {
+        return score->second;
+    } else if (grid[row][col] == 9) {
+        return set<int>{row*n_col + col};
+    }
+
+    set<int> total_scores;
+
+    for (auto dir: DIRECTIONS) {
+        int nrow = row + dir[0];
+        int ncol = col + dir[1];
+        if (0 <= nrow && nrow < grid.size() && 0 <= ncol && ncol < grid[0].size() && grid[row][col] + 1 == grid[nrow][ncol]) {
+            set<int> s = count_routes(nrow, ncol, grid, scores);
+            total_scores.insert(s.begin(), s.end());
+        }
+    }
+
+    scores[key] = total_scores;
+    return total_scores;
+}
+
+int count_routes_2(int row, int col, vector<vector<int>> grid, map<Point_t, int> scores) {
     Point_t key = {row,col};
     auto score = scores.find(key);
 
@@ -36,14 +63,13 @@ int count_routes(int row, int col, vector<vector<int>> grid, map<Point_t, int> s
         return 1;
     }
 
-    int total_scores = 0;
+    int total_scores = 0; 
 
     for (auto dir: DIRECTIONS) {
         int nrow = row + dir[0];
         int ncol = col + dir[1];
         if (0 <= nrow && nrow < grid.size() && 0 <= ncol && ncol < grid[0].size() && grid[row][col] + 1 == grid[nrow][ncol]) {
-            int s = count_routes(nrow, ncol, grid, scores);
-            if (s > 0) total_scores++;
+            total_scores += count_routes_2(nrow, ncol, grid, scores);
         }
     }
 
@@ -54,15 +80,31 @@ int count_routes(int row, int col, vector<vector<int>> grid, map<Point_t, int> s
 void part1(vector<vector<int>> grid) {
 
     int total_score = 0;
-    map<Point_t, int> scores;
+    map<Point_t, set<int>> scores;
 
     for (int row=0; row < grid.size(); row++) {
         for (int col=0; col < grid[0].size(); col++) {
             if (grid[row][col] == 0) {
-                total_score += count_routes(row, col, grid, scores);
+                total_score += count_routes(row, col, grid, scores).size();
             }
         }
     }
 
     cout << "part1: " << total_score << endl;
+}
+
+void part2(vector<vector<int>> grid) {
+
+    int total_score = 0;
+    map<Point_t, int> scores;
+
+    for (int row=0; row < grid.size(); row++) {
+        for (int col=0; col < grid[0].size(); col++) {
+            if (grid[row][col] == 0) {
+                total_score += count_routes_2(row, col, grid, scores);
+            }
+        }
+    }
+
+    cout << "part2: " << total_score << endl;
 }
