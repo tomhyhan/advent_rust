693d39123e851d254721226b8eb84d40c6b6918e
tomhyhan <tomhyhan@gmail.com>
2024-12-18 13:20:01 -0800

    day17

--- advent-2024/src/solutions/day17.cpp
+++ advent-2024/src/solutions/day17.cpp
@@ -1,33 +1,40 @@
 #include "day.hpp"
 
-
 class Computer {
     public:
-        int pointer = 0;
-        int regA, regB, regC;
-        
-        Computer(vector<int> regs) {
+        int64 pointer = 0;
+        int64 regA, regB, regC;
+        string outs;
+
+        Computer(vector<int64> regs) {
             regA = regs[0];
             regB = regs[1];
             regC = regs[2];
         }
 
-        void run_program(vector<int> program) {
-            string outs;
-            while (pointer < program.size()) {
-                optional<int> output = execute(program[pointer],program[pointer+1]);
+        void reset() {
+            pointer = 0;
+            regA = 0;
+            regB = 0;
+            regC = 0;        
+            outs = "";
+        }
 
+        void run_program(vector<int64> program, bool out_once=false) {
+            
+            while (pointer < program.size()) {
+                optional<int64> output = execute(program[pointer],program[pointer+1]);
                 if (output.has_value()) {
-                    // cout << *output << endl;
-                    outs+= to_string(*output) + ',';
+                    outs += to_string(*output);
+                    if (out_once) {
+                        return;
+                    }
                 }
                 pointer += 2;
             }
-
-            cout << outs << endl;
         }
 
-        optional<int> execute(int opcode, int operand) {
+        optional<int64> execute(int64 opcode, int64 operand) {
             switch (opcode) {
                 case 0:
                     regA = regA / pow(2, combo(operand));
@@ -60,7 +67,7 @@ class Computer {
             return nullopt; 
         }
 
-        int combo(int operand) {
+        int64 combo(int64 operand) {
             switch (operand) {
                 case 0:
                 case 1:
@@ -79,12 +86,12 @@ class Computer {
             }
         }
 };
-void part1(Computer computer, vector<int> program);
-
+void part1(Computer computer, vector<int64> program);
+void part2(Computer computer, vector<int64> program);
 
 string Day17::solve(stringstream& input_buffer) {
     
-    vector<int> regs;
+    vector<int64> regs;
     string line;
     size_t pos;
 
@@ -96,7 +103,7 @@ string Day17::solve(stringstream& input_buffer) {
     getline(input_buffer, line);
     pos = line.find(':');
 
-    vector<int> program;
+    vector<int64> program;
     string snum;
     istringstream isline(line.substr(pos+2));
     while (getline(isline, snum, ',')) {
@@ -104,12 +111,45 @@ string Day17::solve(stringstream& input_buffer) {
     }
     Computer computer(regs);
     part1(computer, program);
+    part2(computer, program);
     return "day17";
 }
 
-void part1(Computer computer, vector<int> program) {
+void part1(Computer computer, vector<int64> program) {
     computer.run_program(program);
-    cout << computer.regB << endl;
-
+    cout << "Part1: " << computer.outs << endl;
 }
 
+// [(5), (46), (369), (2953, 2960)]
+void part2(Computer computer, vector<int64> program) {
+
+    vector<int64> reg_values;
+    vector<int64> answers;
+    reg_values.push_back(5);
+    
+    for (int64 i=program.size()-1; i >= 0; i--) {
+        int64 result = program[i];
+        vector<int64> new_reg_values;
+        for (auto reg_v: reg_values)  {
+            computer.reset();
+            computer.regA = reg_v;
+            computer.run_program(program, true);
+            if (computer.outs == to_string(result)) {
+                for (int j = 0; j < 8; j++) {
+                    new_reg_values.push_back(reg_v*8+j);
+                }
+                if (i==0) {
+                    answers.push_back(reg_v);
+                }
+            }
+        }
+        reg_values = new_reg_values;
+    }
+    int64 min_reg_v = *min_element(answers.begin(), answers.end());
+
+    computer.reset();
+    computer.regA = min_reg_v;
+    computer.run_program(program);
+    cout << "Part2: " <<  min_reg_v << endl;
+    cout << "Part2: " <<  computer.outs << endl;
+}
